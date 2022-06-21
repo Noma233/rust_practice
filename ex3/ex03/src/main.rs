0482ddfda0341b8ab989a34c49bd25e31f06c3fe
noma233 <putongnor0@gmail.com>
2022-06-21 20:15:18 +0900

    add new file

---
+++ ex3/ex03/src/main.rs
@@ -0,0 +1,19 @@
+struct AdjacencyList{
+    row_value:Vec<i32>,
+    column_value:Vec<i32>,
+    list:Vec<Vec<i32>>,    
+}
+
+impl AdjacencyList{
+    printMatrix(&self, list_name: String) {
+        println!("{}", list_name);
+
+        
+
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
